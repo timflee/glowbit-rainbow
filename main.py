5430f90160193370c8312be45994becd6ec9bed9
timflee <timflee@hotmail.com>
2022-09-20 23:51:21 +1000

    Minor updates. Use button A to reset!

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 def on_button_pressed_a():
-    global index
-    index += 10
+    rainbow.show()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-index = 20
-rainbow = neopixel.create(DigitalPin.P0, 13, NeoPixelMode.RGB)
+def on_button_pressed_b():
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_logo_pressed():
+    pass
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+rainbow: neopixel.Strip = None
+index = 0
+numLeds = 0
+rainbow = neopixel.create(DigitalPin.P16, 13, NeoPixelMode.RGB)
 rainbow.set_brightness(20)
-rainbow.show_rainbow(1, 360)
+rainbow.clear()
+rainbow.show()
 
 def on_forever():
-    strip: neopixel.Strip = None
-    rainbow.clear()
-    strip.show_bar_graph(100, 255)
-    rainbow.show()
+    pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,34 @@
 input.onButtonPressed(Button.A, function () {
-    rainbow.show()
+    rainbow.clear()
+    index = 0
+    numLeds = 0
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
 })
 let rainbow: neopixel.Strip = null
-let index = 0
 let numLeds = 0
+let index = 0
+index = 0
+numLeds = 0
+let test = 0
 rainbow = neopixel.create(DigitalPin.P16, 13, NeoPixelMode.RGB)
 rainbow.setBrightness(20)
 rainbow.clear()
 rainbow.show()
+basic.showLeds(`
+    . # . . .
+    # . # . .
+    . # . # .
+    . . # . #
+    . . . # .
+    `)
 basic.forever(function () {
     if (index % 100 == 0) {
-        rainbow.setPixelColor(numLeds, neopixel.colors(NeoPixelColors.Orange))
+        rainbow.showBarGraph(numLeds, 12)
         rainbow.show()
         numLeds += 1
     }

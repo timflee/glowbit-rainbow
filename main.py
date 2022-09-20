5385cea52ea240c4be9143bdf513c015d366233a
timflee <timflee@hotmail.com>
2022-09-21 00:06:19 +1000

    Hard coded all rainbow colours to appear over the 50 cycles

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def on_button_pressed_a():
-    rainbow.show()
+    global index, numLeds
+    rainbow.clear()
+    index = 0
+    numLeds = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
@@ -11,13 +14,43 @@ def on_logo_pressed():
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 rainbow: neopixel.Strip = None
+numLeds = 0
+index = 0
+test = 0
 index = 0
 numLeds = 0
+colours =   [neopixel.rgb(255, 0, 0),
+            neopixel.rgb(255, 128, 0),
+            neopixel.rgb(255, 255, 0),
+            neopixel.rgb(128, 255, 0),
+            neopixel.rgb(0, 255, 0),
+            neopixel.rgb(0, 255, 128),
+            neopixel.rgb(0, 255, 255),
+            neopixel.rgb(0, 128, 255),
+            neopixel.rgb(0, 0, 255),
+            neopixel.rgb(128, 0, 255),
+            neopixel.rgb(255, 0, 255),
+            neopixel.rgb(255, 0, 128),
+            neopixel.rgb(255, 255, 255),
+            ]
+
 rainbow = neopixel.create(DigitalPin.P16, 13, NeoPixelMode.RGB)
 rainbow.set_brightness(20)
 rainbow.clear()
 rainbow.show()
+basic.show_leds("""
+    . # . . .
+        # . # . .
+        . # . # .
+        . . # . #
+        . . . # .
+""")
 
 def on_forever():
-    pass
+    global numLeds, index
+    if index % 10 == 0:
+        rainbow.set_pixel_color(numLeds, colours[numLeds])
+        rainbow.show()
+        numLeds += 1
+    index += 1
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -12,9 +12,24 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 let rainbow: neopixel.Strip = null
 let numLeds = 0
 let index = 0
+let test = 0
 index = 0
 numLeds = 0
-let test = 0
+let colours = [
+neopixel.rgb(255, 0, 0),
+neopixel.rgb(255, 128, 0),
+neopixel.rgb(255, 255, 0),
+neopixel.rgb(128, 255, 0),
+neopixel.rgb(0, 255, 0),
+neopixel.rgb(0, 255, 128),
+neopixel.rgb(0, 255, 255),
+neopixel.rgb(0, 128, 255),
+neopixel.rgb(0, 0, 255),
+neopixel.rgb(128, 0, 255),
+neopixel.rgb(255, 0, 255),
+neopixel.rgb(255, 0, 128),
+neopixel.rgb(255, 255, 255)
+]
 rainbow = neopixel.create(DigitalPin.P16, 13, NeoPixelMode.RGB)
 rainbow.setBrightness(20)
 rainbow.clear()
@@ -27,8 +42,8 @@ basic.showLeds(`
     . . . # .
     `)
 basic.forever(function () {
-    if (index % 100 == 0) {
-        rainbow.showBarGraph(numLeds, 12)
+    if (index % 50 == 0) {
+        rainbow.setPixelColor(numLeds, colours[numLeds])
         rainbow.show()
         numLeds += 1
     }
